6c7a7c90fb820988e5372adb4c6aade103c32558
matt mclaurine <matt@caitlain.com>
2019-02-20 15:03:29 -0600

    update echos

--- sync-media/sync-audible.sh
+++ sync-media/sync-audible.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rsync -av --delete-after --no-perms /media/SED/audible/ pi@pi4:/media/SED/audible
-rsync -av --delete-after --no-perms /media/SED/audible/ pi@pi3:/media/SED/audible
-rsync -av --delete-after --no-perms /media/SED/audible/ pi@pi2:/media/SED/audible
-rsync -av --delete-after --no-perms /media/SED/audible/ matt@office:/media/SED/audible
+echo pi4
+rsync -av --delete-after /media/SED/audible pi@pi4:/media/SED/
+echo pi3
+rsync -av --delete-after /media/SED/audible pi@pi3:/media/SED/
+echo pi2
+rsync -av --delete-after /media/SED/audible pi@pi2:/media/SED/
 
--- sync-media/sync-jazz.sh
+++ sync-media/sync-jazz.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rsync -av --delete-after /media/SED/jazz/ pi@pi4:/media/SED/jazz
-rsync -av --delete-after /media/SED/jazz/ pi@pi3:/media/SED/jazz
-rsync -av --delete-after /media/SED/jazz/ pi@pi2:/media/SED/jazz
-rsync -av --delete-after /media/SED/jazz/ matt@office:/media/SED/jazz
+rsync -av --delete-after /media/SED/jazz pi@pi4:/media/SED/
+rsync -av --delete-after /media/SED/jazz pi@pi3:/media/SED/
+rsync -av --delete-after /media/SED/jazz pi@pi2:/media/SED/
 
